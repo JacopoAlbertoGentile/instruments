c63fdaa4d17d61d81bc43e4b5fafcc109bc40312
JacopoAlbertoGentile <jacopoalbertogentile@users.noreply.github.com>
2021-02-27 14:52:33 -0800

    added black and sholes

---
+++ derivatives/black_sholes.py
@@ -0,0 +1,92 @@
+import numpy as np
+import datetime as dt
+import math as m
+import scipy.stats as st
+
+
+class BlackScholes(object):
+    def __init__(self, strike, underlying_price, rate, expiry, type, vol=None):
+        self.strike = strike
+        self.underlying = underlying_price
+        self.vol = vol
+        self.rate = rate
+        self.type = type
+        today = dt.datetime.today()
+        T = dt.datetime.strptime(expiry, "%Y-%m-%d")
+        T = (T - today).days / 252
+        self.T = T
+        self.discount_factor = m.exp(-self.rate * self.T)
+        self.d1 = None
+        self.d2 = None
+
+    def price(self, vol):
+        d1 = (m.log(self.underlying / self.strike) + (self.rate + 0.5 * vol ** 2) * self.T) / (vol * m.sqrt(self.T))
+        d2 = d1 - (vol) * m.sqrt(self.T)
+        if self.type == 'Call' or self.type == "C":
+            call = st.norm.cdf(d1, 0, 1) * self.underlying - self.discount_factor * self.strike * st.norm.cdf(d2, 0, 1)
+            return call
+        else:
+            put = st.norm.cdf(-d2, 0, 1) * self.strike * self.discount_factor - st.norm.cdf(-d1, 0, 1) * self.underlying
+            return put
+
+    def delta(self, vol):
+        d1 = (m.log(self.underlying / self.strike) + (self.rate + 0.5 * vol ** 2) * self.T) / (
+                vol * m.sqrt(self.T))
+        d2 = d1 - (vol) * m.sqrt(self.T)
+        if self.type == "Call" or self.type == "C":
+            deltaBS = st.norm.cdf(d1, 0, 1)
+            return (deltaBS)
+        else:
+            deltaBS = st.norm.cdf(-d1, 0, 1)
+            return (deltaBS)
+
+    def gamma(self, vol):
+        d1 = (m.log(self.underlying / self.strike) + (
+                self.rate+ 0.5 * vol ** 2) * self.T) / (
+                     vol * m.sqrt(self.T))
+        return (st.norm.pdf(d1)) / (vol * self.underlying * m.sqrt(self.T))
+
+
+    def vega(self, vol):
+        d1 = (m.log(self.underlying / self.strike) + (self.rate + 0.5 * vol ** 2) * self.T) / (
+                vol * m.sqrt(self.T))
+        d2 = d1 - (vol) * m.sqrt(self.T)
+        vega_value = self.underlying * st.norm.cdf(d1, 0, 1) * m.sqrt(self.T)
+        return vega_value
+
+    def implied_vol(self, option_mkt_price, vol_est):
+        print((self.price(vol_est), self.delta(vol_est), self.vega(vol_est)))
+        i = 1
+        while (i <= 100):
+            vol_est = vol_est - ((self.price(vol_est) - option_mkt_price) / self.vega(vol_est))
+            if abs(self.price(vol_est) - option_mkt_price) <= 0.0001:
+                print("Numbers of attemps:", i)
+                print("Implied_Vol:", vol_est)
+                return vol_est
+            else:
+                i = i + 1
+
+    def volga(self, vol):
+        Vega = self.vega(vol)
+        d1 = (m.log(self.underlying / self.strike) + (self.rate + 0.5 * vol ** 2) * self.T) / (
+                vol * m.sqrt(self.T))
+        d2 = d1 - (vol) * m.sqrt(self.T)
+        volga = Vega * (d1 * d2) / vol
+        return (volga)
+
+    def vanna(self, vol):
+        d1 = (m.log(self.underlying / self.strike) + (self.rate + 0.5 * vol ** 2) * self.T) / (
+                vol * m.sqrt(self.T))
+        dNorm = m.exp(-0.5 * d1 ** 2) * (1 / 2 * m.pi)
+        vanna = m.sqrt(self.T) * dNorm * (1 - d1)
+        return (vanna)
+
+    def theta(self, vol):
+        d1 = (m.log(self.underlying / self.strike) + (self.rate + 0.5 * vol ** 2) * self.T) / (
+                vol * m.sqrt(self.T))
+        d2 = d1 - (vol) * m.sqrt(self.T)
+        bs_theta = - (self.underlying * st.norm.pdf(d1) * vol * 0.5 / m.sqrt(self.T)) - self.rate * self.strike * self.discount_factor * st.norm.cdf(d2, 0, 1)
+        return bs_theta
+
+
+
--- derivatives/options.py
+++ derivatives/options.py
@@ -1,49 +1,62 @@
-
+from derivatives.black_sholes import BlackScholes
 
 class Options(object):
     def __init__(self):
         self.strike = None
         self.price = None
         self.expire = None
-        self.underlying = None
+        self.underlying_price = None
 
     @classmethod
-    def call(cls, strike, price, expire, underlying):
+    def call(cls, strike, price, expiry, underlying_price, rate):
         functions_args_values = locals()
         option = cls()
+        bs = BlackScholes(
+            strike = strike,
+            underlying_price=underlying_price,
+            rate=rate,
+            expiry=expiry,
+            type='C'
+        )
 
         class Call(object):
             def __init__(self):
                 self._option = option
+                self.bs=bs
                 for arg, value in functions_args_values.items():
                     if hasattr(self._option, arg):
                         setattr(self, arg, value)
                     else:
                         pass
 
-            def __class_name__(self):
-                return "Call"
+            def payoff(self, underlying_price):
+                return (underlying_price - self.strike) - self.price
 
         return Call()
 
     @classmethod
-    def put(cls, strike, price, expire, underlying):
+    def put(cls, strike, price, expiry, underlying_price, rate):
         functions_args_values = locals()
         option = cls()
+        bs = BlackScholes(
+            strike = strike,
+            underlying_price=underlying_price,
+            rate=rate,
+            expiry=expiry,
+            type='P'
+        )
 
         class Put(object):
             def __init__(self):
                 self._option = option
+                self.bs=bs
                 for arg, value in functions_args_values.items():
                     if hasattr(self._option, arg):
                         setattr(self, arg, value)
                     else:
                         pass
 
-            def payoff(self):
+            def payoff(self, underlying_price):
+                return (self.strike - underlying_price) - self.price
 
         return Put()
-
-if __name__=="__main__":
-    call = Options.call(1,1,1,1)
-    print(call.__class__)
